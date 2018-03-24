5df18eddb19848d4650498d9ea258e9265196bca
Niall Mahford <niall.mahford@gmail.com>
2018-03-24 14:22:47 -0800

    Configure logout and spinner in App.

--- react_native/auth/src/App.js
+++ react_native/auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null }
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyBHRL7EXWy6Mg4MCAkgEQqixo3x8qS2-rg',
@@ -14,16 +16,48 @@ class App extends Component {
             storageBucket: 'authentication-78eed.appspot.com',
             messagingSenderId: '157127907897'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else { 
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <View style={styles.logOutButtonStyle}>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </View>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner />;
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    logOutButtonStyle: {
+        padding: 5,
+        flexDirection: 'row'
+    }
+};
 
 export default App;
