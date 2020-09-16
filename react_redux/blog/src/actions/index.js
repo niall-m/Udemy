e0f7e0e1e7a25dba4b123d7312d878d0b86544f0
Niall Mahford <niall.mahford@gmail.com>
2020-09-16 16:41:51 -0700

    async actions with redux thunk

---
+++ react_redux/blog/src/actions/index.js
@@ -0,0 +1,16 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response })
+};
+
+// functionally equivalent
+// export const fetchPosts = () => {
+//   return async function(dispatch, getState) {
+//     const response = await jsonPlaceholder.get('/posts');
+  
+//     dispatch({ type: 'FETCH_POSTS', payload: response })
+//   }
+// };
---
+++ react_redux/blog/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  replaceMe: () => 'not a valid reducer for redux'
+});
