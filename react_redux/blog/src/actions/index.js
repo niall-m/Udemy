612b50f302a468e3b5414afe94d33bc92ed2053e
Niall Mahford <niall.mahford@gmail.com>
2020-09-18 11:36:21 -0700

    redux store design and async action creators

--- react_redux/blog/src/actions/index.js
+++ react_redux/blog/src/actions/index.js
@@ -1,16 +1,38 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// When dispatching a function, thunk automatically invokes it
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // execute
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response })
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// functionally equivalent
-// export const fetchPosts = () => {
-//   return async function(dispatch, getState) {
-//     const response = await jsonPlaceholder.get('/posts');
-  
-//     dispatch({ type: 'FETCH_POSTS', payload: response })
-//   }
-// };
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// // Alternative: memoize to reduce API calls, but can only fetch once per user
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
--- react_redux/blog/src/reducers/index.js
+++ react_redux/blog/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-  replaceMe: () => 'not a valid reducer for redux'
+  posts: postsReducer,
+  users: usersReducer
 });
