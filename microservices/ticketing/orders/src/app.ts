3124d7ae4c40d7fde3cb8de53ea58a5c31287f83
Niall Mahford <niall.mahford@gmail.com>
2021-01-11 12:53:04 -0500

    initial setup for orders service

---
+++ microservices/ticketing/orders/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@nmgittickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true); // ingress-nginx
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disable encryption
+    secure: process.env.NODE_ENV !== 'test' // enforce https unless in jest test env
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
