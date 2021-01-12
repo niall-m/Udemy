286df5b36ac47f49d9a7a8529b2ab4512ec3ae12
Niall Mahford <niall.mahford@gmail.com>
2021-01-12 16:40:56 -0500

    order created and cancelled event publishers, tests

--- microservices/ticketing/orders/src/routes/delete.ts
+++ microservices/ticketing/orders/src/routes/delete.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from 'express';
 import { NotAuthorizedError, NotFoundError, requireAuth } from '@nmgittickets/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
   const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
-
+  const order = await Order.findById(orderId).populate('ticket');
+  
   if(!order) {
     throw new NotFoundError();
   }
@@ -20,6 +22,12 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
   await order.save();
   
   // publish event saying this was cancelled
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id
+    }
+  });
   
   res.status(204).send(order);
 });
