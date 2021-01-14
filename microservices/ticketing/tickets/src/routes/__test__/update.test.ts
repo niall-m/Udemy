b671aba412a9b8912ef5c588d19f8bd1e7975e99
Niall Mahford <niall.mahford@gmail.com>
2021-01-14 16:59:34 -0500

    wire up listeners, reject edits of reserved tickets, test

--- microservices/ticketing/tickets/src/routes/__test__/update.test.ts
+++ microservices/ticketing/tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
-import { app } from '../../app';
 import mongoose from 'mongoose';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
@@ -126,4 +127,30 @@ it('publishes an event', async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates if the ticket is reserved', async () => {
+  // create a cookie, create a ticket, fetch ticket and set orderId, try to update ticket, expect 400 bad request
+  const cookie = global.getAuthCookie();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'whatever',
+      price: 20
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Bad Request Update',
+      price: 2000
+    })
+    .expect(400);
 });
--- microservices/ticketing/tickets/src/routes/update.ts
+++ microservices/ticketing/tickets/src/routes/update.ts
@@ -4,7 +4,8 @@ import {
   validateRequest,
   NotFoundError,
   requireAuth,
-  NotAuthorizedError
+  NotAuthorizedError,
+  BadRequestError
 } from '@nmgittickets/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
@@ -28,6 +29,10 @@ router.put('/api/tickets/:id',
 
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket');
     }
 
     ticket.set({
