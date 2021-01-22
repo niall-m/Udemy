08af03e4d45433979a1c9823b9ea42c42caa2857
Niall Mahford <niall.mahford@gmail.com>
2021-01-22 18:18:13 -0500

    header links, filter reserved orders, redirect after purchase

--- microservices/ticketing/client/pages/orders/[orderId].js
+++ microservices/ticketing/client/pages/orders/[orderId].js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
@@ -8,7 +9,7 @@ const OrderShow = ({ order, currentUser }) => {
     url: '/api/payments',
     method: 'post',
     body: { orderId: order.id },
-    onSuccess: (payment) => console.log(payment)
+    onSuccess: () => Router.push('/orders')
   });
 
   useEffect(() => {
