f184039d5bbf119b0fc19932656119486ffa079d
Niall Mahford <niall.mahford@gmail.com>
2021-01-21 20:18:08 -0500

    order timer and expiration

--- microservices/ticketing/client/pages/orders/[orderId].js
+++ microservices/ticketing/client/pages/orders/[orderId].js
@@ -1,7 +1,36 @@
-const OrderShow = () => {
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft(); // invoke immediately, setInterval won't
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    }
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+  
   return (
-    <div>OrderShow</div>
+    <div>Time left to pay: {timeLeft} seconds</div>
   );
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
 }
 
 export default OrderShow;
